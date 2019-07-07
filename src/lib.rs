bedff18999531f884555b46ef8d857fadf4ac70c
Jordan Earls <earlz@earlz.net>
2019-07-06 23:10:53 -0600

    Minimal messy decoding

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 extern crate lazy_static;
 
 #[allow(dead_code)] //remove after design stuff is done
-mod qx86{
+pub mod qx86{
 
     const MAX_ARGS:usize = 3;
 
@@ -18,10 +18,10 @@ mod qx86{
     enum ValueSource{
         None,
         ModRM,
+        ModRMReg, //the /r field
         ImmediateAddress,
         ImmediateValue,
-        Register,
-        RegisterAddress
+        RegisterSuffix //lowest 3 bits of the opcode is used for register
     }
 
     #[derive(Copy, Clone)]
@@ -32,6 +32,7 @@ mod qx86{
         Conditional
     }
 
+    #[derive(PartialEq)]
     enum Register{
         EAX,
         ECX,
@@ -73,6 +74,7 @@ mod qx86{
         function: OpcodeFn,
         arg_size: [ValueSize; MAX_ARGS],
         arg_source: [ValueSource; MAX_ARGS],
+        has_modrm: bool,
         gas_cost: i32,
         rep_valid: bool,
         size_override_valid: bool,
@@ -80,6 +82,7 @@ mod qx86{
         jump_behavior: JumpBehavior
     }
 
+    #[derive(PartialEq)]
     enum ValueLocation{
         None,
         Immediate(u32),
@@ -114,11 +117,20 @@ mod qx86{
                 function: nop,
                 arg_size: [ValueSize::None, ValueSize::None, ValueSize::None],
                 arg_source: [ValueSource::None, ValueSource::None, ValueSource::None],
+                has_modrm: false,
                 gas_cost: 0,
                 rep_valid: false,
                 size_override_valid: false,
                 address_override_valid: false,
                 jump_behavior: JumpBehavior::None
+            }
+        }
+    }
+    impl Default for OpArgument{
+        fn default() -> OpArgument{
+            OpArgument{
+                location: ValueLocation::None,
+                size: ValueSize::None
             }
         }
     }
@@ -131,12 +143,93 @@ mod qx86{
         };
     }
 
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    //Rust doesn't support bitfields directly (and workarounds are not great)
+    //So, just unpack into bytes
+    struct ModRM{
+        mode: u8, //actually called mod, but can't call it that here
+        reg: u8,
+        rm: u8
     }
+
+    fn convert_reg_to_address(reg: u8, size: ValueSize) -> u32{
+        0
+    }
+
+    fn decode_args(opcode: &Opcode, bytestream: &[u8], args: &mut [OpArgument; MAX_ARGS], address_override: bool) -> Result<usize, usize>{
+        use ValueSource::*;
+        use std::convert::TryInto;
+        let opcode_byte = bytestream[0];
+        let mut bytes = &bytestream[0..];
+        let mut size = 0;
+        size += match opcode.arg_source[0] {
+            None => {
+                0
+            },
+            ModRM => {
+                1
+            },
+            ModRMReg => {
+                1
+            },
+            ImmediateAddress =>{
+                if address_override{
+                    bytes = &bytes[1..]; //advance by one
+                        let b: [u8; 2] = match &bytes.try_into(){
+                        Ok(res) => *res,
+                        Err(_) => return Err(size)
+                    };
+                    args[0].location = ValueLocation::Address(u16::from_le_bytes(b) as u32);
+                    args[0].size = opcode.arg_size[0];
+                    2
+                } else {
+                    bytes = &bytes[1..]; //advance by one
+                    let b: [u8; 4] = match &bytes.try_into(){
+                        Ok(res) => *res,
+                        Err(_) => return Err(size)
+                    };
+                    args[0].location = ValueLocation::Address(u32::from_le_bytes(b));
+                    args[0].size = opcode.arg_size[0];
+                    4
+                }
+            }
+            ImmediateValue => {
+                2
+            },
+            RegisterSuffix =>{
+                args[0].location = ValueLocation::Address((opcode_byte & 0x7) as u32);
+                0
+            }
+        };
+        Ok(size)
+    }
+
+
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        #[test]
+        fn it_works() {
+            assert_eq!(2 + 2, 4);
+        }
+        #[test]
+        fn decode_immediate_address(){
+            let mut args:[OpArgument; MAX_ARGS] = Default::default();
+            let mut opcode:Opcode = Default::default();
+            opcode.arg_source[0] = ValueSource::ImmediateAddress;
+            let bytes:&[u8] = &[
+                0xFA, //the opcode
+                0x11, //argument begin
+                0x22,
+                0x33,
+                0x44]; //argument end
+            
+            match decode_args(&opcode, bytes, &mut args, false){
+                Err(_) => assert!(false, "decode resulted in error"),
+                Ok(s) => assert_eq!(s, 4)
+            };
+            assert!(args[0].location == ValueLocation::Address(0x44332211));
+        }
+    }
+
 }
