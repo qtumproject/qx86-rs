1cb640cecce1759d068142e3f5fd759e186af1e1
VoR0220 <catalanor0220@gmail.com>
2019-07-18 12:44:21 -0500

    rename flag names
    Signed-off-by: VoR0220 <catalanor0220@gmail.com>

--- src/flags.rs
+++ src/flags.rs
@@ -1,64 +1,64 @@
 pub struct X86Flags {
-    pub carry_flag: bool,
-    pub parity_flag: bool,
-    pub adjust_flag: bool,
-    pub zero_flag: bool,
-    pub sign_flag: bool,
-    pub trap_flag: bool,
-    pub interrupt_flag: bool,
-    pub direction_flag: bool,
-    pub overflow_flag: bool,
-    pub iopl_flag: bool,
-    pub nested_task_flag: bool,
-    pub resume_flag: bool,
-    pub virtual_8086_mode_flag: bool,
-    pub alignment_check_flag: bool,
-    pub id_capability_flag: bool,
-    pub vif_flag: bool,
-    pub vip_flag: bool
+    pub carry: bool,
+    pub parity: bool,
+    pub adjust: bool,
+    pub zero: bool,
+    pub sign: bool,
+    pub trap: bool,
+    pub interrupt: bool,
+    pub direction: bool,
+    pub overflow: bool,
+    pub iopl: bool,
+    pub nested_task: bool,
+    pub resume: bool,
+    pub virtual_8086_mode: bool,
+    pub alignment_check: bool,
+    pub id_capability: bool,
+    pub vif: bool,
+    pub vip: bool
 }
 
 impl X86Flags {
-    pub fn calculate_parity_flag(&mut self, u32 val) {
+    pub fn calculate_parity(&mut self, u32 val) {
         let mut count = 0;
         for i in 0..7 {
             if ((val&((1<<i)))!=0){
                 count++;
             }
         }
-        self.parity_flag = (count % 2) == 0;
+        self.parity = (count % 2) == 0;
     }
-    pub fn calculate_sign_flag8(&mut self, u8 val) {
+    pub fn calculate_sign8(&mut self, u8 val) {
         if (val & 0x80) == 0 {
-            self.sign_flag = true;
+            self.sign = true;
         } else {
-            self.sign_flag = false;
+            self.sign = false;
         }
     }
-    pub fn calculate_sign_flag16(&mut self, u16 val) {
+    pub fn calculate_sign16(&mut self, u16 val) {
         if (val & 0x8000) == 0 {
-            self.sign_flag = true;
+            self.sign = true;
         } else {
-            self.sign_flag = false;
+            self.sign = false;
         }
     }
-    pub fn calculate_sign_flag32(&mut self, u32 val) {
+    pub fn calculate_sign32(&mut self, u32 val) {
         if (val & 0x80000000) == 0 {
-            self.sign_flag = true;
+            self.sign = true;
         } else {
-            self.sign_flag = false;
+            self.sign = false;
         }
     }
-    pub fn calculate_overflow_flag8(&mut self, u8 result, u8 val1, u8 val2) {
-        self.overflow_flag = !((v1^v2) & 0x80) && ((v1^result) & 0x80);
+    pub fn calculate_overflow8(&mut self, u8 result, u8 val1, u8 val2) {
+        self.overflow = !((v1^v2) & 0x80) && ((v1^result) & 0x80);
     }
-    pub fn calculate_overflow_flag16(&mut self, u16 result, u16 val1, u16 val2) {
-        self.overflow_flag = !((v1^v2) & 0x80) && ((v1^result) & 0x8000);
+    pub fn calculate_overflow16(&mut self, u16 result, u16 val1, u16 val2) {
+        self.overflow = !((v1^v2) & 0x80) && ((v1^result) & 0x8000);
     }
-    pub fn calculate_overflow_flag32(&mut self, u32 result, u32 val1, u32 val2) {
-        self.overflow_flag =  !((v1^v2) & 0x80) && ((v1^result) & 0x80000000);
+    pub fn calculate_overflow32(&mut self, u32 result, u32 val1, u32 val2) {
+        self.overflow =  !((v1^v2) & 0x80) && ((v1^result) & 0x80000000);
     }
-    pub fn calculate_zero_flag(&mut self, u32 result) {
-        self.zero_flag = result == 0;
+    pub fn calculate_zero(&mut self, u32 result) {
+        self.zero = result == 0;
     }
 }
