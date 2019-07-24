9137e4dac2a3d90941a1ae00490031e3897ce763
Jordan Earls <earlz@earlz.net>
2019-07-23 21:42:09 -0600

    Add module level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,19 @@ extern crate strum;
 #[macro_use]
 extern crate strum_macros;
 
+/// Helper structures used across the entire VM and sometimes externally
 pub mod structs;
+/// Instruction and argument decoding supports
 pub mod decoding;
+/// Opcode definitions including the giant master opcode map for the subset of x86 that qx86 implements
 pub mod opcodes;
+/// Execution pipeline building supports
 pub mod pipeline;
+/// Primary VM interface and functions which opcode logic functions can use to modify and retrieve the VM state
 pub mod vm;
+/// Memory support used by the VM to track virtual mmemory
 pub mod memory;
+/// The actual opcode logic function implementations
 mod ops;
 
 
